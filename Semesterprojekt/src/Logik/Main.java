63089a832a7d9782faea1df97b401b071d612c3c
Matias <3020772@stud.hs-mannheim.de>
2025-01-04 15:59:34 +0100

    .

---
+++ Semesterprojekt/src/Logik/Main.java
@@ -0,0 +1,14 @@
+package Logik;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		try {
+			TicTacToe ttt = new TicTacToe();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
