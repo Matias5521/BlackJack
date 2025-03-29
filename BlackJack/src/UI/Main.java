548698f0cfc88d8d9f052399331a7d64874899ec
Matias <3020772@stud.hs-mannheim.de>
2025-03-29 11:45:38 +0100

    Aufteilung der Main Klasse in UserInterface und Main

--- BlackJack/src/UI/Main.java
+++ BlackJack/src/UI/Main.java
@@ -1,62 +1,13 @@
 package UI;
-import java.util.InputMismatchException;
-
-import java.util.Scanner;
 
 import Logik.BlackJackSpiel;
 
 
 public class Main {
-	
-	private static BlackJackSpiel bjs;
 
 	public static void main(String[] args) {
 		
-		bjs = new BlackJackSpiel();
+		UserInterface ui = new UserInterface(new BlackJackSpiel());
 		
-		System.out.println("Das Spiel wurde gestartet.\nEs kann nur ein Spieler spielen und dass auch nur einmal\nUm erneut zu spielen muss das Programm neu gestartet werden.");
-		
-		bjs.getNeueHand();
-		
-		Scanner s = new Scanner(System.in);
-		
-		while(!bjs.isEnde()) {
-			
-			ausgabeAktuellerStand();
-			
-			if (bjs.pruefeBlackJack()) {
-				System.out.println("Es wurde ein Blackjack erreicht:\n"+bjs.ausgabeHand());
-				System.exit(0);
-			}
-			
-			System.out.println("Wollen Sie eine weitere Karte ziehen? (j/n)");
-			
-			try {
-				String input = s.nextLine();
-				
-				if(input.equalsIgnoreCase("j")) {
-					bjs.fuegeKarteHinzu();
-					ausgabeAktuellerStand();
-				}else if(input.equalsIgnoreCase("n")) {
-					System.out.println("Das Ergebnis des Versuchs ist: "+bjs.ausgabePunkteKarte());
-					break;
-				}else{
-					throw new InputMismatchException();
-				}
-				
-				if(bjs.pruefeEnde()) {
-					bjs.verteilePunkteNeu();
-					System.out.println("Es wurden 21 oder mehr Punkte erreicht, der Versuch ist beendet!\nDas Ergebnis ist: "+bjs.ausgabePunkteKarte()); 
-					break;
-				}
-					
-			}catch(InputMismatchException e){
-				System.out.println("Falsche Eingabe, versuchen Sie es erneut!");
-			}
-		}
-	}
-	
-	private static void ausgabeAktuellerStand() {
-		System.out.println("Ihr aktueller Stand:\n"+bjs.getHand().toString());
 	}
 }
